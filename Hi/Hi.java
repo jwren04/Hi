69c3ba3617774a2c8b692cb72919bb87ae578c4e
Jennie Ren <jennie.ren@lvjusd.k12.ca.us>
2021-11-04 14:39:53 -0700

    i added another line

--- Hi/Hi.java
+++ Hi/Hi.java
@@ -3,5 +3,6 @@ public class Hi
     public static void main(){
         System.out.println("Hello");
         System.out.println("have an amazing day");
+        System.out.println("hi hola");
     }
 }
